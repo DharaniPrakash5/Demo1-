4c0ac2ab8e0b6acf2186b61aed5f1d93b0c67e4c
DharaniPrakash5 <90841408+DharaniPrakash5@users.noreply.github.com>
2026-02-20 13:23:02 +0530

    Update ProjectSpecificMethod.java

--- src/test/java/com/learnpom/base/ProjectSpecificMethod.java
+++ src/test/java/com/learnpom/base/ProjectSpecificMethod.java
@@ -2,17 +2,19 @@ package com.learnpom.base;
 
 import java.time.Duration;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class ProjectSpecificMethod {
 	//java.lang.NullPointerException:
+	
 	public static RemoteWebDriver driver;
 	@BeforeMethod
 	public void preCondition() {
-		driver = new FirefoxDriver();
+		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get("http://leaftaps.com/opentaps/control/main");
