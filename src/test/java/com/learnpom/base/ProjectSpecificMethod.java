13e4487102b420c40456aa1c514d2d9c2d76f13c
DharaniPrakash5 <90841408+DharaniPrakash5@users.noreply.github.com>
2026-02-21 14:26:17 +0530

    Update ProjectSpecificMethod.java

--- src/test/java/com/learnpom/base/ProjectSpecificMethod.java
+++ src/test/java/com/learnpom/base/ProjectSpecificMethod.java
@@ -3,7 +3,7 @@ package com.learnpom.base;
 import java.time.Duration;
 
 import org.openqa.selenium.edge.EdgeDriver;
-
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,12 +14,20 @@ public class ProjectSpecificMethod {
 	public static RemoteWebDriver driver;
 	@BeforeMethod
 	public void preCondition() {
-		driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	    EdgeOptions options = new EdgeOptions();
+
+	    options.addArguments("--headless=new");
+	    options.addArguments("--disable-gpu");
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
+	    options.addArguments("--window-size=1920,1080");
+
+	    driver = new EdgeDriver(options);
+
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	    driver.get("http://leaftaps.com/opentaps/control/main");
 	}
-	
 	@AfterMethod
 	public void postCondition() {
 		driver.quit();
